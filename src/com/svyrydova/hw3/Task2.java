4a0a9a2b2d6f63786b8d819dfb31922351954bb2
Viktoriia Svyrydova <11minut25@gmail.com>
2021-10-31 00:31:11 +0300

    add hw1 add hw2 add hw3 ===

--- src/com/svyrydova/hw3/Task2.java
+++ src/com/svyrydova/hw3/Task2.java
@@ -4,34 +4,34 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        int cancel;
-        while (cancel != 0) {
-
+        String cancel = "";
+        while (true) {
+            if (cancel == "0") {
+                break;
+            }
             String request1 = "Введите символ для вычисления";
             System.out.println(request1);
             Scanner symb = new Scanner(System.in);
-            char a = symb.next().charAt(0);
-            cancel = Character.getNumericValue(a);
+            String a = symb.nextLine();
+            cancel = a;
             System.out.println("Введите x");
-            Scanner num1 = new Scanner(System.in);
-            int x = num1.nextInt();
+            int x = symb.nextInt();
             System.out.println("Введите y");
-            Scanner num2 = new Scanner(System.in);
-            int y = num2.nextInt();
+            int y = symb.nextInt();
             switch (a) {
-                case ('+'):
+                case ("+"):
                     int z1 = x + y;
                     System.out.println(z1);
                     break;
-                case ('-'):
+                case ("-"):
                     int z2 = x - y;
                     System.out.println(z2);
                     break;
-                case ('*'):
+                case ("*"):
                     int z3 = x * y;
                     System.out.println(z3);
                     break;
-                case ('/'):
+                case ("/"):
                     if (y == 0) {
                         System.out.println("Делить на ноль нельзя");
                     } else {
@@ -43,6 +43,6 @@ public class Task2 {
                     System.out.println("Вы ввели неправильный символ");
             }
         }
-
     }
 }
+
