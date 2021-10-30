e9588e9792d21a9ed53b6aacac9efe08fdebb19c
Viktoriia Svyrydova <11minut25@gmail.com>
2021-10-31 00:41:57 +0300

    add hw3 final task 2

--- src/com/svyrydova/hw3/Task2.java
+++ src/com/svyrydova/hw3/Task2.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        String cancel = "";
+
         while (true) {
-            if (cancel == "0") {
-                break;
-            }
+
             String request1 = "Введите символ для вычисления";
             System.out.println(request1);
             Scanner symb = new Scanner(System.in);
             String a = symb.nextLine();
-            cancel = a;
+            if (a.equals("0")) {
+                break;
+            }
             System.out.println("Введите x");
             int x = symb.nextInt();
             System.out.println("Введите y");
